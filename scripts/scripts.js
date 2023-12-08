b71f59e211ef905540338c19c859abc7ee30d356
Comichimera <149447638+Comichimera@users.noreply.github.com>
2023-12-08 14:07:25 +0000

    SCP-290 Overhaul

---
+++ scripts/scripts.js
@@ -0,0 +1,8 @@
+function toggleVisibility(elementId) {
+    var content = document.getElementById(elementId);
+    if (content.style.display === "none") {
+        content.style.display = "block";
+    } else {
+        content.style.display = "none";
+    }
+}
